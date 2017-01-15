26c5d2b0ee9a305e89ce9c02eb1e87137b8db508
Yury Kuptsou <yurasb@tut.by>
2017-01-15 20:09:44 +0300

    Adding waiting

--- new_windows.py
+++ new_windows.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get(
@@ -18,9 +19,11 @@ main_window = driver.current_window_handle
 ext_links = driver.find_elements_by_css_selector(
     'td>a[target=\'_blank\']'
 )
+wait = WebDriverWait(driver, 10)
 for link in ext_links:
     link.click()
     current_windows = driver.window_handles
+    wait.until(EC.number_of_windows_to_be(2))
     driver.switch_to_window(current_windows[1])
     driver.close()
     driver.switch_to_window(main_window)
